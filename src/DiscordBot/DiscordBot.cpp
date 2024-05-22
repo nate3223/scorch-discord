d66123dbcdc1a75100d7e7192120969f2657006a
Nate Rearick <nathanice3223@gmail.com>
2024-05-23 01:06:20 +1000

    Added advanced button example

--- src/DiscordBot/DiscordBot.cpp
+++ src/DiscordBot/DiscordBot.cpp
@@ -1,16 +1,23 @@
 #include "DiscordBot.hpp"
 
-DiscordBot::DiscordBot(const char* token)
-    : m_bot(token)
-{
-    m_bot.on_log(dpp::utility::cout_logger());
+#include <dpp/unicode_emoji.h>
 
+DiscordBot::DiscordBot(const char* token)
+    : m_bot(token, dpp::i_all_intents)
+{
+    m_slashCommands = {
+            { "ping", "Ping pong!", std::bind_front(&DiscordBot::onPingCommand, this) },
+            { "yippee", "Yippee!", std::bind_front(&DiscordBot::onYippeeCommand, this) },
+            { "setstatuschannel", "Sets the channel where the server status is displayed.", std::bind_front(&DiscordBot::onSetStatusChannel, this) }
+    };
+
+    m_bot.on_log(dpp::utility::cout_logger());
     m_bot.on_slashcommand([this](const dpp::slashcommand_t& event){ onSlashCommand(event); });
 
     m_bot.on_ready([this](const dpp::ready_t& event) {
         if (dpp::run_once<struct register_bot_commands>()) {
-            m_bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", m_bot.me.id));
-            m_bot.global_command_create(dpp::slashcommand("yippee", "Yippee!", m_bot.me.id));
+            for (const auto& command : m_slashCommands)
+                m_bot.global_command_create(dpp::slashcommand(command.command, command.description, m_bot.me.id));
         }
     });
 }
@@ -22,12 +29,42 @@ void DiscordBot::start()
 
 void DiscordBot::onSlashCommand(const dpp::slashcommand_t& event)
 {
-    if (event.command.get_command_name() == "ping")
-    {
-        event.reply("Pong!");
-    }
-    else if (event.command.get_command_name() == "yippee")
-    {
-        event.reply("Yippee!");
-    }
+    const auto& commandName = event.command.get_command_name();
+    const auto it = std::find_if(m_slashCommands.begin(), m_slashCommands.end(),
+        [commandName](const SlashCommand& command)
+        {
+            return commandName == command.command;
+        }
+    );
+    if (it != m_slashCommands.end())
+        it->execute(event);
+}
+
+void DiscordBot::onPingCommand(const dpp::slashcommand_t &event)
+{
+    event.reply("Pong!");
+}
+
+void DiscordBot::onYippeeCommand(const dpp::slashcommand_t &event)
+{
+    event.reply("Yippee!!");
+}
+
+void DiscordBot::onSetStatusChannel(const dpp::slashcommand_t& event)
+{
+    const auto guild = event.command.guild_id;
+    const auto channel = event.command.channel_id;
+    m_serverStatusChannel.insert_or_assign(guild, channel);
+    dpp::message msg(channel, "Penis message");
+    msg.add_component(
+        dpp::component().add_component(
+            dpp::component()
+                .set_label("Meow!")
+                .set_type(dpp::cot_button)
+                .set_emoji(dpp::unicode_emoji::cat)
+                .set_style(dpp::cos_primary)
+                .set_id("statuschannel")
+        )
+    );
+    m_bot.message_create(msg);
 }
--- src/DiscordBot/DiscordBot.hpp
+++ src/DiscordBot/DiscordBot.hpp
@@ -1,7 +1,15 @@
 #pragma once
 
 #include <dpp/dpp.h>
-#include <string.h>
+#include <vector>
+#include <unordered_map>
+
+struct SlashCommand
+{
+    const char* command;
+    const char* description;
+    std::function<void(const dpp::slashcommand_t&)> execute;
+};
 
 class DiscordBot
 {
@@ -12,6 +20,11 @@ public:
 
 private:
     void    onSlashCommand(const dpp::slashcommand_t& event);
+    void    onPingCommand(const dpp::slashcommand_t& event);
+    void    onYippeeCommand(const dpp::slashcommand_t& event);
+    void    onSetStatusChannel(const dpp::slashcommand_t& event);
 
-    dpp::cluster    m_bot;
+    dpp::cluster                m_bot;
+    std::vector<SlashCommand>   m_slashCommands;
+    std::unordered_map<dpp::snowflake, dpp::snowflake>  m_serverStatusChannel;
 };
